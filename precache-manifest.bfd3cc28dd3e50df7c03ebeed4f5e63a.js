a1a59fd3990459fd6c4c405a7417d7cc1bb1e5bd
Claire Filipek <cfilipek@sva.edu>
2019-11-04 14:21:09 -0500

    Updates

---
+++ precache-manifest.bfd3cc28dd3e50df7c03ebeed4f5e63a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c035c1b16f979daf9bf228e9bf150cd9",
+    "url": "/Codevember/index.html"
+  },
+  {
+    "revision": "259d0012dad42a2154ad",
+    "url": "/Codevember/static/css/main.7f232cc7.chunk.css"
+  },
+  {
+    "revision": "8e31ce07b3acbe15b855",
+    "url": "/Codevember/static/js/2.8d208f4a.chunk.js"
+  },
+  {
+    "revision": "259d0012dad42a2154ad",
+    "url": "/Codevember/static/js/main.fb1272b6.chunk.js"
+  },
+  {
+    "revision": "a3c2450c1fc49d2bffef",
+    "url": "/Codevember/static/js/runtime-main.e4c59e88.js"
+  }
+]);
