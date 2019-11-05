b3b4e019b765f9ad6e9cb7c0b3bd60a64f1358bf
Claire Filipek <cfilipek@sva.edu>
2019-11-04 19:58:39 -0500

    Fixed input field clear in geometric project

--- src/components/Geometric.js
+++ src/components/Geometric.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Motion, spring } from 'react-motion';
 import Navbar from './Navbar';
 import { Row, Col, Button } from 'react-bootstrap'
+import { Form } from 'react-advanced-form';
+import { Input } from 'react-advanced-form-addons';
 
 
 class Geometric extends Component {
@@ -10,15 +12,20 @@ class Geometric extends Component {
   };
 
   handleClick = val => {
-    if (val && !isNaN(val)) {
+;    if (val && !isNaN(val)) {
       this.setState({
         left: +val
       });
     }
   };
 
-  reset = () => this.setState({ left: 0 });
+  reset = () => {this.setState({ left: 0 })
+    this.form.reset()}
 
+  changeHandler = event => {
+    console.log(event);
+      this.form = event.nextValue;
+  }
   render() {
     return (
       <React.Fragment>
@@ -29,20 +36,22 @@ class Geometric extends Component {
           style={{ left: spring(this.state.left) }}
         >
           {val => (
-            <div className="geometric-box" style={{ position: 'absolute', ...val, marginTop: 100 }}></div>
+            <div className="geometric-box" style={{ position: 'absolute', ...val, marginTop: 200 }}></div>
           )}
         </Motion>
         <div className="spacer"/>
         <Row>
           <Col md={4} />
           <Col md={4}>
-            <input
-              type="number"
-              placeholder="enter a value"
-              ref={input => (this.input = input)}
-            />
-            <Button style={{ marginLeft: 20 }} onClick={() => this.handleClick(this.input.value)}>Set</Button>
-            <Button style={{ marginLeft: 20 }} onClick={this.reset}>Reset</Button>
+            <Form ref={form => (this.form = form)}>
+              <Input
+                type="number"
+                placeholder="enter a value"
+                onChange={this.changeHandler}
+              />
+               <Button style={{ marginLeft: 20 }} onClick={() => this.handleClick(parseInt(this.form))}>Set</Button>
+               <Button style={{ marginLeft: 20 }} onClick={(this.reset)}>Reset</Button>
+            </Form>
           </Col>
           <Col md={4} />
         </Row>
